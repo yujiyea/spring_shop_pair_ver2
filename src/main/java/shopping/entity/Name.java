39d1c6363bd0271867519360689298c987f9bb62
yujiyea <yje3002@naver.com>
2024-08-27 16:13:01 +0900

    refactor: name 검증로직이 누락된 부분 수정

--- src/main/java/shopping/entity/Name.java
+++ src/main/java/shopping/entity/Name.java
@@ -10,14 +10,15 @@ public class Name {
     }
 
     public Name(String value) {
+        isVaild(value);
         this.value = value;
     }
 
-    private void isVaild() {
-        if (this.value == null) {
+    private void isVaild(String value) {
+        if (value == null) {
             throw new IllegalArgumentException("상품명이 null로 입력되었습니다.");
         }
-        if (this.value.length() >= 15) {
+        if (value.length() >= 15) {
             throw new IllegalArgumentException("상품의 이름은 15자를 넘길 수 없습니다.");
         }
     }
